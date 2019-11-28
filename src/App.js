a0b5262ba02d2aa058c058e8b84caae155d09b0d
Koua Thao <kouathao09@gmail.com>
2019-11-28 00:43:39 -0600

    completed video 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./logo.png";
+
+// components
 import TodosList from "./components/todos-list.component";
 import EditTodo from "./components/edit-todo.component";
 import CreateTodo from "./components/create-todo.component";
@@ -10,7 +13,37 @@ class App extends Component {
       // Router elemebt
       <Router>
         <div className="container">
-          <h2>MEARN-Stack Todo App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a
+              className="navbar-brand"
+              href="http://www.kouathao.com"
+              target="_blank"
+            >
+              <img
+                src={logo}
+                width="30"
+                height="30"
+                alt="CodingTheSmartWay.com"
+              />
+            </a>
+            <Link to="/" className="navbar-brand">
+              MERN-Stack Todo App
+            </Link>
+            <div className="collpase nav-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">
+                    Create Todo
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
           <Route path="/" exact component={TodosList} />
           <Route path="/edit/:id" exact component={EditTodo} />
           <Route path="/create" exact component={CreateTodo} />
